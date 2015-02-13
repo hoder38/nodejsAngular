bd040d3d4a5a9b7aa4a7c7f5239b4b687e9c3d2a
hoder38 <hoder3388@gmail.com>
2015-02-13 10:38:38 +0800

    add config

---
+++ config/dev.js
@@ -0,0 +1,18 @@
+module.exports = {
+    ip: '114.32.213.158',
+    port: 8080,
+    wss_port: 8084,
+    ws_port: 8085,
+    wsj_port: 8086,
+    db_name: 'mydb',
+    db_ip: 'localhost',
+    db_port: 27017,
+    session_name: 'mySessionStore',
+    session_ip: 'localhost',
+    session_port: 27017,
+    xuite_parent_key: '777f8390c31dc934d2f6ee0d5af37afc',
+    google_media_folder: '0B_BstyDfOj4Rfk54SHNvU2ExV3RJTGlvWHljWTRHSXprcU0tM0o5QXR6c1dfWlNiVTV6VlE',
+    google_backup_folder: '0B_BstyDfOj4RfnU3RWRldjl0RGV4YUxxZlJTOEV1c1dDemRpa2c2cFphRDQ3bnI2M2RnblU',
+    nas_tmp: "/mnt/tmp",
+    nas_prefix: "/mnt/storage"
+};
---
+++ config/release.js
@@ -0,0 +1,18 @@
+module.exports = {
+    ip: '114.32.213.158',
+    port: 443,
+    wss_port: 8081,
+    ws_port: 8082,
+    wsj_port: 8083,
+    db_name: 'releasedb',
+    db_ip: 'localhost',
+    db_port: 27017,
+    session_name: 'releaseSessionStore',
+    session_ip: 'localhost',
+    session_port: 27017,
+    xuite_parent_key: '777f8390c31dc934d2f6ee0d5af37afc',
+    google_media_folder: '0B_BstyDfOj4Rfk54SHNvU2ExV3RJTGlvWHljWTRHSXprcU0tM0o5QXR6c1dfWlNiVTV6VlE',
+    google_backup_folder: '0B_BstyDfOj4RfnU3RWRldjl0RGV4YUxxZlJTOEV1c1dDemRpa2c2cFphRDQ3bnI2M2RnblU',
+    nax_tmp: "/mnt/tmp",
+    nax_prefix: "/mnt/release/storage"
+};
