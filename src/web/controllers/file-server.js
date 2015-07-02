8df5efef4765b93c115a866550c58cf25c3abde6
hoder38 <hoder3388@gmail.com>
2015-07-02 23:35:15 +0800

    file server 7/2

--- src/web/controllers/file-server.js
+++ src/web/controllers/file-server.js
@@ -9,3 +9,157 @@ var config_type = require('../../../ver.js');
 
 var config_glb = require('../../../config/' + config_type.dev_type + '.js');
 
+var mongo = require("../models/mongo-tool.js");
+
+var util = require("../util/utility.js");
+
+var http = require('http'),
+    express = require('express'),
+    crypto = require('crypto'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
+    app = express(),
+    serverHttp = http.createServer(app),
+    //port = 80,
+    encode = "utf8",
+    sessionStore = require("../models/session-tool.js")(express);
+
+app.use(express.favicon());
+app.use(express.cookieParser());
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(express.session(sessionStore.config));
+app.use(require('connect-multiparty')({ uploadDir: config_glb.nas_tmp }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/api/logout', function(req, res, next) {
+    console.log("logout");
+    console.log(new Date());
+    console.log(req.url);
+    console.log(req.body);
+    if (req.isAuthenticated()) {
+        //req.logout();
+        req.session.destroy();
+    }
+    //res.clearCookie('id');
+    res.json({apiOK: true});
+});
+
+//passport
+passport.use(new LocalStrategy(function(username, password, done){
+    //記得檢查input!!!
+    console.log('login');
+    console.log(username);
+    var name = util.isValidString(username, 'name'),
+        pwd = util.isValidString(password, 'passwd');
+    if (name === false) {
+        util.handleError({hoerror: 2, message: "username is not vaild"}, done);
+        done(null, false, { message: "username is not vaild" });
+    } else {
+        if (pwd === false) {
+            util.handleError({hoerror: 2, message: "passwd is not vaild"}, done);
+            done(null, false, { message: "passwd is not vaild" });
+        } else {
+            mongo.orig("find", "user" ,{username: name}, {limit: 1}, function(err,users){
+                if(err) {
+                    util.handleError(err, done, function(err) {
+                        return done(err);
+                    });
+                }
+                if(users.length === 0){
+                    return done(null, false, { message: "Incorrect username." });
+                }
+                var encodePwd = crypto.createHash('md5').update(pwd).digest('hex');
+                if (encodePwd === users[0].password) {
+                    return done(null, users[0]);
+                }
+                return done(null, false, { message: 'Incorrect password.' });
+            });
+        }
+    }
+}));
+
+passport.serializeUser(function(user, done) {
+    done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+    mongo.orig("find", "user", {_id: mongo.objectID(id)}, {limit: 1}, function(err,users){
+        if(err) {
+            util.handleError(err, done, function(err) {
+                return done(err);
+            });
+        }
+        done(null,users[0]);
+    });
+});
+
+//api error handle
+app.post('/api*', passport.authenticate('local', { failureRedirect: '/api' }),
+    function(req, res) {
+        console.log("auth ok");
+        res.json({loginOK: true, id: req.user.username});
+});
+
+app.all('/api*', function(req, res, next) {
+    "use strict";
+    console.log('auth fail!!!');
+    console.log(new Date());
+    console.log(req.url);
+    console.log(req.body);
+    res.send('auth fail!!!', 401);
+});
+
+app.all('*', function(req, res, next) {
+    "use strict";
+    console.log('page not found');
+    console.log(new Date());
+    console.log(req.url);
+    console.log(req.body);
+    //console.log(req.path);
+    res.send('Page not found!', 404);
+});
+
+//error handle
+app.use(function(err, req, res, next) {
+    "use strict";
+    util.handleError(err);
+    res.send('server error occur', 500);
+});
+
+process.on('uncaughtException', function(err) {
+    console.log('Threw Exception: %s  %s', err.name, err.message);
+    if (err.stack) {
+        console.log(err.stack);
+    }
+});
+
+serverHttp.listen(3389, config_glb.ip);
+
+function checkLogin(req, res, next, callback) {
+    if(!req.isAuthenticated()){
+        if (util.isMobile(req.headers['user-agent'])) {
+            if (/^\/video\//.test(req.path)) {
+                console.log("mobile");
+                setTimeout(function(){
+                    callback(req, res, next);
+                }, 0);
+            } else {
+                next();
+            }
+        } else {
+            next();
+        }
+    } else {
+        console.log(req.user._id);
+        setTimeout(function(){
+            callback(req, res, next);
+        }, 0);
+    }
+}
+
+
+console.log('start express server\n');
+
+console.log("Server running at http://" + config_glb.ip + ":" + 3389 + ' ' + new Date());
