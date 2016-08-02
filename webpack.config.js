45d0ee057494a609e89a020fe08fa9d07a0d8219
hoder38 <hoder3388@gmail.com>
2016-08-02 14:49:15 +0800

    react

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
-const webpack = require('webpack');
-require('babel-polyfill');
+var webpack = require('webpack');
 
 module.exports = {
     entry: ['babel-polyfill', './src/front/index.js'],
@@ -9,11 +8,31 @@ module.exports = {
     },
     module: {
         loaders: [
-            //{ test: /\.css$/, loader: 'style!css' },
             { test: /\.jsx?$/,
                 exclude: /node_modules/,
                 loader: 'babel'
-            }
+            },
+            { test: /\.css$/,
+                loader: 'style!css'
+            },
+            { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/font-woff'
+            },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=application/octet-stream'
+            },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file'
+            },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url?limit=10000&mimetype=image/svg+xml'
+            },
+            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: "url?limit=10000&minetype=application/font-woff"
+            },
+            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: "file"
+            },
         ]
     },
     plugins: [
