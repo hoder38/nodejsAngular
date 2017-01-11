1f20c66d0c7071d3b717fbacd8b0925f78cf934d
hoder38 <hoder3388@gmail.com>
2017-01-11 14:04:21 +0800

    bugfix

--- src/web/models/mediaHandle-tool.js
+++ src/web/models/mediaHandle-tool.js
@@ -10,7 +10,7 @@ var mongo = require("../models/mongo-tool.js");
 
 var mime = require('../util/mime.js');
 
-var Transcoder = require('../util/stream-transcoder.js'),
+var Transcoder = require('../util/stream-transcoder.js');
 
 var child_process = require('child_process'),
     path = require('path'),
