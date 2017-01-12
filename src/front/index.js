dff692848a0fad016d15d0350904536eaecab683
hoder38 <hoder3388@gmail.com>
2017-01-12 10:04:58 +0800

    move css fonts

--- src/front/index.js
+++ src/front/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
-import '../css/bootstrap.min.css'
-import '../css/bootstrap-theme.min.css'
-import '../css/style.css'
+import './css/bootstrap.min.css'
+import './css/bootstrap-theme.min.css'
+import './css/style.css'
 import Root from './containers/Root'
 
 render(
